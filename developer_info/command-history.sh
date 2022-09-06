89868c28f47dac3085b64dbc42ed7a9adca1a1e9
Amlan Alok <amlanalok@gmail.com>
2022-09-06 00:31:20 -0500

    command history added

---
+++ developer_info/command-history.sh
@@ -0,0 +1,54 @@
+amlanalok % npx create-react-app amlanalok.com
+
+Creating a new React app in /Users/amlanalok/Documents/95-website-amlanalok/amlanalok.com.
+
+Installing packages. This might take a couple of minutes.
+Installing react, react-dom, and react-scripts with cra-template...
+
+
+added 1396 packages in 38s
+
+209 packages are looking for funding
+  run `npm fund` for details
+
+
+#   ------------------------------------------------
+# Use this command to package the project
+npm run build
+#   ------------------------------------------------
+
+amlanalok@Amlans-MacBook-Pro amlanalok.com % npm run build
+
+> amlanalok.com@0.1.0 build
+> react-scripts build
+
+Creating an optimized production build...
+Compiled with warnings.
+
+[eslint] 
+src/App.js
+  Line 1:8:  'logo' is defined but never used  no-unused-vars
+
+Search for the keywords to learn more about each warning.
+To ignore, add // eslint-disable-next-line to the line before.
+
+File sizes after gzip:
+
+  46.47 kB  build/static/js/main.49826c7d.js
+  1.79 kB   build/static/js/787.2850f8fb.chunk.js
+  541 B     build/static/css/main.073c9b0a.css
+
+The project was built assuming it is hosted at /.
+You can control this with the homepage field in your package.json.
+
+The build folder is ready to be deployed.
+You may serve it with a static server:
+
+  npm install -g serve
+  serve -s build
+
+Find out more about deployment here:
+
+  https://cra.link/deployment
+
+amlanalok@Amlans-MacBook-Pro amlanalok.com % 
