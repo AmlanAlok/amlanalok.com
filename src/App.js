efd21dbddf6906689e15e76e56f2abb90ecf85e8
Amlan Alok <amlanalok@gmail.com>
2023-02-19 21:11:05 -0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home.js'
 import Resume from './components/Resume/Resume'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
     <Routes>
       <Route path='/' element = {<Home />}/>
       <Route path='resume' element = {<Resume />}/>
     </Routes>
+    </>
   );
 }
 
