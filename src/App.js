7896b793c812e1a0c4f8cf9c3baf6065cba55cab
Amlan Alok <amlanalok@gmail.com>
2023-02-19 21:28:12 -0600

    No Match URL condition added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home.js'
 import Resume from './components/Resume/Resume'
 import Navbar from './components/Navbar/Navbar'
+import NoMatch from './components/NoMatch/NoMatch'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element = {<Home />}/>
       <Route path='resume' element = {<Resume />}/>
+      <Route path='*' element={<NoMatch />}/>
     </Routes>
     </>
   );
