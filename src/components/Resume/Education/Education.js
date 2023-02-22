3b960af50de9f1ba06847b123d077a7d99952748
Amlan Alok <amlanalok@gmail.com>
2023-02-21 18:23:54 -0600

    educatoin component done

--- src/components/Resume/Education/Education.js
+++ src/components/Resume/Education/Education.js
@@ -2,10 +2,24 @@
 function Education() {
     return (
         <div>
-            Education
-            
-            University of Texas, Arlington (GPA - 4/4)
-            Master’s in Computer Science (Specialization in Artificial Intelligence and Database)
+            <h2>Education</h2>
+
+            <div className="row">
+                <div className="col-sm-8">
+                    <h4><strong>University of Texas, Arlington (GPA - 4/4)</strong></h4>
+                </div>
+                <div className="col-sm-4">
+                    <h5 className='float-right'>January 2021 - May 2023</h5>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-12">
+                    <p>Master’s in Computer Science (Specialization in Artificial Intelligence and Database)</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
