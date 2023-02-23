70bcbb495f9463719483b6bc9d0faa4cd17c58b2
Amlan Alok <amlanalok@gmail.com>
2023-02-22 23:52:28 -0600

    justified text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,8 +8,8 @@ function Home() {
             <header>
                 <h1 className='center-pos'>Welcome to Amlan Alok's website</h1>
                 <div className='row'>
-                    <div className="col-sm-8">
-                        
+                    <div className="col-sm-8 justify-txt">
+
                         <h2>Introduction</h2>
                         <h4>I am currently an international graduate student majoring in Computer Science at University of Texas, Arlington. I am in my final semester and will be graduating on 15th May, 2023.</h4>
                         <h4>I wish to work in the American software industry after I graduate in May 2023 and hence I am looking for job opportunities in USA.</h4>
@@ -21,8 +21,8 @@ function Home() {
                         <h4>My experience taught me about taking accountability, managing high pressure, try to exceed expectations, mentoring new joiners, presenting work to clients, managing time critical production issues, and to be strong in times of crisis.</h4>
                         <h4>But all good things come to an end and my client’s project scope was going to end in December of 2020. Hence instead of joining a new project, I decided to pursue higher education to learn about emerging technologies, broaden my scope of thinking, hone my skills and improve my future prospects.</h4>
 
-                    </div>
-                    <div className="col-sm-4">
+                    </div> 
+                    <div className="col-sm-4 justify-txt">
                         <h2>For Recruiters</h2>
                         <h4>If you are a recruiter or someone who knows of a possible job opportunity, and you believe that my experience and skill set: </h4>
 
