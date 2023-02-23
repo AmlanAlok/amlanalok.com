8bbbc694ea0d12d0961f099da4aeb8b5bd0f892c
Amlan Alok <amlanalok@gmail.com>
2023-02-23 04:46:04 -0600

    Proj and cert added

--- src/components/Resume/Certifications/Certifications.js
+++ src/components/Resume/Certifications/Certifications.js
@@ -3,6 +3,11 @@ function Certifications() {
     return (
         <div>
             <h2>Certifications</h2>
+            <h4>
+                <ul>
+                    <li>Cognizant Academy Full Stack Engineer Program Certificate (2020)</li>
+                </ul>
+            </h4>
         </div>
     )
 }
--- src/components/Resume/Projects/Projects.js
+++ src/components/Resume/Projects/Projects.js
@@ -3,6 +3,18 @@ function Projects() {
     return (
         <div>
             <h2>Projects</h2>
+            <h4>
+                <ul>
+                    <li>Developed <strong>AI agent</strong> to play <strong>Max Connect 4</strong> using <strong>depth limited minimax search with alpha-beta pruning</strong></li>
+                    <li>Implemented several <strong>ML Algorithms from scratch</strong> without using ML libraries - Decision Tree, Bagging,
+Hierarchical Clustering, Linear Regression, Gaussian Naive Bayes, KNN</li>
+                    <li><strong>Housing Portal Website - Tech Stack -</strong> Spring Boot 2, Java, API, ReactJS, SQL, Socket.io, NodeJS
+                        <ul>
+                            <li>Developed full stack app in three member team. Designed database, implemented chat messaging, dashboards, reports, charts, login, sign-up, e-mail notifications.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </h4>
         </div>
     )
 }
