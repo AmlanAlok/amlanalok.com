9ef9baa1553b3b15f78ba870d6a6ed283e082472
Amlan Alok <amlanalok@gmail.com>
2023-05-05 03:13:55 -0500

    Full Stack Program Certificate added with link

--- src/components/Resume/Certifications/Certifications.js
+++ src/components/Resume/Certifications/Certifications.js
@@ -5,7 +5,9 @@ function Certifications() {
             <h2>Certifications</h2>
             <h4>
                 <ul>
-                    <li>Cognizant Academy Full Stack Engineer Program Certificate (2020)</li>
+                    <li>Cognizant Academy Full Stack Engineer Program Certificate 2020 
+                        (<a href='Full-Stack-Engineer-Program.png' target='_blank' rel="noreferrer">Link</a>)
+                    </li>
                 </ul>
             </h4>
         </div>
