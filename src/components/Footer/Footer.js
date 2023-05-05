ebc11ade70fb54f8e679aa0c73bffbe902a53be9
Amlan Alok <amlanalok@gmail.com>
2023-05-05 05:27:53 -0500

    version updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
         <div className='footer-area '>
             <div className='row'>
                 <span className='footer-font'>
-                    <h5>Version 1.1</h5>
+                    <h5>Version 1.2</h5>
                 </span>
             </div>
         </div>
