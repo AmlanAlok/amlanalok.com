6f78167201f2a1ea04bf364a773e3548c47c1da6
Amlan Alok <amlanalok@gmail.com>
2024-06-16 21:03:22 -0500

    made the entire code flexible

--- src/components/ProgrammingLanguages/ProgrammingLanguages.js
+++ src/components/ProgrammingLanguages/ProgrammingLanguages.js
@@ -1,14 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
 import DisplayCode from "./DisplayCode"
 
 function ProgrammingLanguages() {
+
+    const [dataArray, setDataArray] = useState([]);
+
+    useEffect(() => {
+        const fetchCSV = async () => {
+            const response = await fetch('/code_files.csv');
+            const reader = response.body.getReader();
+            const result = await reader.read(); // raw array
+            const decoder = new TextDecoder('utf-8');
+            const csv = decoder.decode(result.value); // the csv text
+            const results = Papa.parse(csv, { header: false });
+            setDataArray(results.data);
+        };
+
+        fetchCSV();
+    }, []);
+
     return (
         <div>
             <h1>Python vs Java vs Javascript</h1>
+            <p>Hi</p>
+            
+            {dataArray.map((row, rowIndex) => (
 
-            <DisplayCode title = {'How to write Hello World?'} 
-            pythonFileName = {'hello_world.py'} 
-            javaFileName = {'HelloWorld.java'} 
-            javascriptFileName = {'HelloWorld.js'}/>
+              <DisplayCode key={rowIndex} title={row[0]}
+                pythonFileName={row[1]}
+                javaFileName={row[2]}
+                javascriptFileName={row[3]} />
+
+          ))}
+
         </div>
     )
 }
